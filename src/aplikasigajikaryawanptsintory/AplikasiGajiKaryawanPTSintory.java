44c189150bf073dd97a619cdfe9093cdeb0fdd50
Mirai Kara <kyuzan03@gmail.com>
2025-05-05 20:04:21 +0700

    add file

---
+++ src/aplikasigajikaryawanptsintory/AplikasiGajiKaryawanPTSintory.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasigajikaryawanptsintory;
+
+import view.FormUtama;
+
+/**
+ *
+ * @author kyuzan
+ */
+public class AplikasiGajiKaryawanPTSintory {
+    private final static FormUtama formUtama = new FormUtama();
+    
+    public static void main (String[] args) {
+    formUtama.setVisible(true);
+}
+}
+
+    
